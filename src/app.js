476d21b83f1695ea8a73b13d901ebfb3ee440264
Greg Shanks <greg.shanks@gmail.com>
2019-12-17 19:50:31 +0000

    MVP and extensions complete - meant to do more commits, whoops!

--- src/app.js
+++ src/app.js
@@ -4,11 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      allRates: [],
-      currencyAmount: 0
+      allRates: {},
+      currency: {
+        amount: null,
+        firstSelected: null,
+        secondSelected: null
+      }
     },
     mounted() {
       this.fetchRates();
+    },
+    computed: {
+      selectedConversion: function() {
+        let result = (this.currency.amount * this.currency.firstSelected);
+        return result.toFixed(2);
+      },
+      euroConversion: function() {
+        let result = (this.currency.amount / this.currency.firstSelected);
+        return result.toFixed(2);
+      },
+      nonEuroConversion: function() {
+        let result = (this.currency.amount / this.currency.firstSelected);
+        result = (result * this.currency.secondSelected);
+        return result.toFixed(2);
+      }
     },
     methods: {
       fetchRates: function() {
